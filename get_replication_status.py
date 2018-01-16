2ec41bf5bad12fe941d78a2cc4e191935ebeea7c
tangweichun <519267852@qq.com>
2018-01-16 18:56:24 +0800

    Update get_replication_status.py

--- get_replication_status.py
+++ get_replication_status.py
@@ -1,17 +1,19 @@
-import pymysql
-from pymysql.cursors import DictCursor
+#/usr/bin/python
+import MySQLdb
+from MySQLdb.cursors import DictCursor
 
 #cursorclass=DictCursor indicates return results as a dictionary
-conn = pymysql.connect(host='192.168.56.100',
+conn = pymysql.connect(host='127.0.0.1',
                        port=3306,
                        user='repl',
-                       password='repl',
-                       database='test',
-                       connect_timeout=10000,
+                       passwd='repllper',
                        cursorclass=DictCursor)
 
 cur = conn.cursor()
 cur.execute('show slave status')
 res=cur.fetchall()
 for row in res:
-    print row['Slave_SQL_Running']
+    if row['Slave_SQL_Running'] =='Yes' and row['Slave_IO_Running'] == 'Yes':
+        print 1
+    else:
+        print 0
